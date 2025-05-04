307411a7c56898868459ab74f3fa1a703fbf3a3e
Xavanamika <132728634+Brick2030@users.noreply.github.com>
2025-05-04 10:40:30 +0700

    making it more readable

--- main.py
+++ main.py
@@ -1,27 +1,17 @@
 import random
 import os
 import time
-
 import pyperclip
-
 import ctypes
 import ctypes.wintypes
-
-import os
-
-
 
 def updateFile(Data):
     output = "\n".join(Data)
     with open('Recs.txt', 'w', encoding='utf-8') as file:
             content = file.write(output)
 
-
-
 def copy_to_clipboard(text):
     pyperclip.copy(text)
-
-
 
 
 while(True):
@@ -39,7 +29,6 @@ while(True):
             if not '|' in i: lines.remove(i)
         print(f"{len(lines)} Entries")
         
-
         for i in range(5):
             rand_stack.append(random.choice(lines))
 
@@ -49,24 +38,23 @@ while(True):
             print(f"<{index}> >>> {subline[1]}\n")
             index += 1
 
-        choice = input("Choose")
+        choice = input("Choosing: ")
 
         if(choice == ''): continue
 
         else:
             try:
-                # Получение дескриптора буфера обмена
-                os.system('cls')
+                os.system('cls' if os.name == 'nt' else 'clear')
                 
                 ChoosedLine = rand_stack[int(choice)-1]
                 subline = ChoosedLine.split('|')
                 print(f"{subline[1]}\n{subline[0]}")
-                # Копирование текста в буфер обмена
 
                 copy_to_clipboard(subline[0])
                 lines.remove(ChoosedLine)
                 updateFile(lines)
                 time.sleep(5)
+            
             except Exception as e:
                 print(e)
             
@@ -78,6 +66,3 @@ while(True):
         break
 
 a = input("Type anything to leave: ")
-
-
-
